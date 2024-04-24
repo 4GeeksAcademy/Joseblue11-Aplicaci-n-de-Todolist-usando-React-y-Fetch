12a719662f9a3f7c449f1329af217a08350d95e3
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2024-04-24 16:51:11 +0000

    Aplicaion Todo List usando  React y Fetch, estuve mejorando la interface de usuario, aun debo trabajar en implementar las funciones Fetch y APIs

---
+++ src/js/component/data.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">!</h1>
+				
+                <img src={rigoImage} />
+			<p>
+				Made by Jose Antonio Tovar with love!
+			</p>
+		</div>
+	);
+};
+
+export default Home;
